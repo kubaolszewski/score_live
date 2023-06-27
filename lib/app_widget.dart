3df714a8c66d0adcb4627677effd26e3bb684527
Jakub Olszewski <kuba.olszewski.0704@icloud.com>
2023-06-27 11:19:59 +0200

    Add auto-generated localizationsDelegates and supportedLocales lists

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:score_live/app/features/landing/landing_module.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class MyApp extends StatelessWidget {
@@ -9,18 +8,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Modular.setInitialRoute(LandingPagePaths.homeModulePath);
+    Modular.setInitialRoute(
+      LandingPagePaths.homeModulePath,
+    );
     return MaterialApp.router(
-      localizationsDelegates: const [
-        AppLocalizations.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      supportedLocales: const [
-        Locale('en'), // English
-        Locale('pl'), // Polish
-      ],
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
       title: 'scorelive',
       theme: ThemeData(
         useMaterial3: true,
