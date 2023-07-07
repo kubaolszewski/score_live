f2a1374010fb36cc5323eb8a1fc787fd16cc62c7
Jakub Olszewski <kuba.olszewski.0704@icloud.com>
2023-07-07 10:01:45 +0200

    Add required changes

--- lib/app/custom_widgets/wide_match_tile/elements/wide_match_tile_elapsed_time.dart
+++ lib/app/custom_widgets/wide_match_tile/elements/wide_match_tile_elapsed_time.dart
@@ -12,22 +12,19 @@ class WideMatchTileElapsedTime extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      flex: 0.5.toInt(),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 24.0, horizontal: 16.0),
-        child: Column(
-          children: [
-            Text(
-              liveMatch.fixture!.status!.short!,
-              style: const TextStyle(color: Colors.grey, fontWeight: FontWeight.bold),
-            ),
-            const Text(
-              '14/8',
-              style: TextStyle(color: Colors.grey, fontWeight: FontWeight.bold),
-            ),
-          ],
-        ),
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 24.0, horizontal: 16.0),
+      child: Column(
+        children: [
+          Text(
+            liveMatch.fixture!.status!.short!,
+            style: const TextStyle(color: Colors.grey, fontWeight: FontWeight.bold),
+          ),
+          const Text(
+            '14/8',
+            style: TextStyle(color: Colors.grey, fontWeight: FontWeight.bold),
+          ),
+        ],
       ),
     );
   }
