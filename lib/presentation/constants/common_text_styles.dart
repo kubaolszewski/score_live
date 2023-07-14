db50bf129ec3e6934dabeb08ca4c4ec65b335102
Jakub Olszewski <kuba.olszewski.0704@icloud.com>
2023-07-14 14:25:25 +0200

    Remove unnecessary text style

--- lib/presentation/constants/common_text_styles.dart
+++ lib/presentation/constants/common_text_styles.dart
@@ -2,5 +2,4 @@ import 'package:flutter/material.dart';
 
 abstract class CommonTextStyles {
   static const basicWhiteText = TextStyle(color: Colors.white, fontWeight: FontWeight.bold);
-  static const basicWhiteTextWithFontSize = TextStyle(color: Colors.white, fontWeight: FontWeight.bold);
 }
