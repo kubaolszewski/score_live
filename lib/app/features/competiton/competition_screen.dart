26b544a152496a46b2d18e19b1a0fb95d5741904
Jakub Olszewski <kuba.olszewski.0704@icloud.com>
2023-07-17 14:47:14 +0200

    Rebuild competition screen

--- lib/app/features/competiton/competition_screen.dart
+++ lib/app/features/competiton/competition_screen.dart
@@ -1,19 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:score_live/app/custom_widgets/custom_app_bar.dart';
-import 'package:score_live/app/features/competiton/competition_tabs/favorites_tab/favorites_tab.dart';
 import 'package:score_live/app/features/competiton/competition_tabs/region_tab/cubit/region_tab_cubit.dart';
-import 'package:score_live/app/features/competiton/competition_tabs/region_tab/region_tab.dart';
 import 'package:score_live/app/features/competiton/competition_tabs/top_results_tab/cubit/top_results_tab_cubit.dart';
-import 'package:score_live/app/features/competiton/competition_tabs/top_results_tab/top_results_tab.dart';
-import 'package:score_live/app/features/competiton/competition_widgets/competition_options_tap_bar.dart';
-import 'package:score_live/app/features/competiton/competition_widgets/competition_search_bar.dart';
+import 'package:score_live/app/features/competiton/competition_view.dart';
 import 'package:score_live/app/features/competiton/cubit/competition_cubit.dart';
-import 'package:score_live/core/applocalization_context.dart';
-import 'package:score_live/core/enums.dart';
-import 'package:score_live/presentation/constants/app_colors.dart';
-import 'package:score_live/presentation/constants/common_text_styles.dart';
 
 class CompetitionScreen extends StatelessWidget {
   const CompetitionScreen({super.key});
@@ -26,36 +17,7 @@ class CompetitionScreen extends StatelessWidget {
         BlocProvider(create: (context) => topResultsTabCubit..fetchLeagues(DateTime.now())),
         BlocProvider(create: (context) => regionTabCubit..fetchLeaguesByRegion(DateTime.now())),
       ],
-      child: Scaffold(
-        backgroundColor: AppColors.backgroundBlack,
-        appBar: CustomAppBar(
-          title: Text(
-            context.localizations.competitionAppBarTitle,
-            style: CommonTextStyles.basicWhiteText,
-          ),
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              CompetitionSearchBar(),
-              const SizedBox(height: 15),
-              const CompetitionOptionsTapBar(),
-              BlocBuilder<CompetitionCubit, CompetitionState>(
-                builder: (context, state) {
-                  switch (state.browsingOptions) {
-                    case BrowsingOptions.top:
-                      return const TopResultsTab();
-                    case BrowsingOptions.region:
-                      return const RegionTab();
-                    case BrowsingOptions.favorites:
-                      return const FavoritesTab();
-                  }
-                },
-              ),
-            ],
-          ),
-        ),
-      ),
+      child: const CompetitionView(),
     );
   }
 }
