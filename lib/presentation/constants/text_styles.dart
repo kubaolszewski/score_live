344b86f86630a0949d477f0fb615c4aa4740f71b
Jakub Olszewski <kuba.olszewski.0704@icloud.com>
2023-07-18 13:45:23 +0200

    Create custom text style with optional parameters

---
+++ lib/presentation/constants/text_styles.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: annotate_overrides, overridden_fields
+
+import 'package:flutter/material.dart';
+
+abstract class CommonTextStyles {
+  static const basicWhiteTextwithWeight = TextStyle(color: Colors.white, fontWeight: FontWeight.w700);
+  static const basicWhiteTextWithFontSizeAndWeight =
+      TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 24);
+}
+
+class CustomTextStyle extends TextStyle {
+  const CustomTextStyle({this.fontSize, required this.color, required this.fontWeight});
+
+  final double? fontSize;
+  final Color color;
+  final FontWeight fontWeight;
+}
