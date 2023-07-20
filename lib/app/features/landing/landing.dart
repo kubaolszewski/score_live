71bd693693f63af0b7e7362b73589733dc13668f
Jakub Olszewski <kuba.olszewski.0704@icloud.com>
2023-07-20 12:54:17 +0200

    Convert landing screen to stateless widget and resize nav bar icons

--- lib/app/features/landing/landing.dart
+++ lib/app/features/landing/landing.dart
@@ -6,13 +6,53 @@ import 'package:score_live/app/features/landing/landing_module.dart';
 import 'package:score_live/core/applocalization_context.dart';
 import 'package:score_live/presentation/constants/app_colors.dart';
 
-class LandingScreen extends StatefulWidget {
+class LandingScreen extends StatelessWidget {
   const LandingScreen({
     super.key,
   });
 
   @override
-  State<LandingScreen> createState() => _LandingScreenState();
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => Modular.get<LandingCubit>(),
+      child: BlocConsumer<LandingCubit, LandingState>(
+        listener: _landingScreenListener,
+        builder: (context, state) {
+          return Scaffold(
+            backgroundColor: AppColors.backgroundBlack,
+            body: const RouterOutlet(),
+            bottomNavigationBar: BottomNavigationBar(
+              currentIndex: state.currentIndex,
+              onTap: Modular.get<LandingCubit>().screenSwitch,
+              items: [
+                BottomNavigationBarItem(
+                  activeIcon: const Icon(Icons.home_filled, size: 28),
+                  icon: const Icon(Icons.home_outlined),
+                  label: context.localizations.navHome,
+                ),
+                BottomNavigationBarItem(
+                  activeIcon: const Icon(Icons.list, size: 28),
+                  icon: const Icon(Icons.table_rows_outlined),
+                  label: context.localizations.navCompetition,
+                ),
+                BottomNavigationBarItem(
+                  activeIcon: const Icon(Icons.person, size: 28),
+                  icon: const Icon(Icons.person_outlined),
+                  label: context.localizations.navAccount,
+                ),
+              ],
+              backgroundColor: AppColors.backgroundBlack,
+              selectedFontSize: 16,
+              unselectedFontSize: 14,
+              selectedLabelStyle: const TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+              selectedItemColor: AppColors.mainThemePink,
+              unselectedItemColor: AppColors.appBarInactiveIcon,
+            ),
+          );
+        },
+      ),
+    );
+  }
 }
 
 void _landingScreenListener(BuildContext context, LandingState state) {
@@ -31,50 +71,4 @@ void _landingScreenListener(BuildContext context, LandingState state) {
 @override
 void dispose() {
   Modular.dispose<LandingCubit>();
-}
-
-class _LandingScreenState extends State<LandingScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => Modular.get<LandingCubit>(),
-      child: BlocConsumer<LandingCubit, LandingState>(
-        listener: _landingScreenListener,
-        builder: (context, state) {
-          return Scaffold(
-            backgroundColor: AppColors.backgroundBlack,
-            body: const RouterOutlet(),
-            bottomNavigationBar: BottomNavigationBar(
-              currentIndex: state.currentIndex,
-              onTap: Modular.get<LandingCubit>().screenSwitch,
-              items: [
-                BottomNavigationBarItem(
-                  activeIcon: const Icon(Icons.home_filled),
-                  icon: const Icon(Icons.home_outlined, size: 32),
-                  label: context.localizations.navHome,
-                ),
-                BottomNavigationBarItem(
-                  activeIcon: const Icon(Icons.list),
-                  icon: const Icon(Icons.table_rows_outlined, size: 32),
-                  label: context.localizations.navCompetition,
-                ),
-                BottomNavigationBarItem(
-                  activeIcon: const Icon(Icons.person),
-                  icon: const Icon(Icons.person_outlined, size: 32),
-                  label: context.localizations.navAccount,
-                ),
-              ],
-              backgroundColor: AppColors.backgroundBlack,
-              selectedFontSize: 16,
-              unselectedFontSize: 16,
-              selectedLabelStyle: const TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
-              unselectedLabelStyle: const TextStyle(fontWeight: FontWeight.bold, color: AppColors.inactiveItemGrey),
-              selectedItemColor: AppColors.mainThemePink,
-              unselectedItemColor: AppColors.inactiveItemGrey,
-            ),
-          );
-        },
-      ),
-    );
-  }
 }
