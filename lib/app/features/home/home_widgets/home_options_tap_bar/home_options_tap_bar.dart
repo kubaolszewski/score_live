a22479bb403ea9671ae20fdb3729ec791c9dc974
Jakub Olszewski <kuba.olszewski.0704@icloud.com>
2023-07-20 13:23:00 +0200

    Apply changes requested in review

--- lib/app/features/home/home_widgets/home_options_tap_bar/home_options_tap_bar.dart
+++ lib/app/features/home/home_widgets/home_options_tap_bar/home_options_tap_bar.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:score_live/app/features/home/cubit/home_cubit.dart';
-import 'package:score_live/app/features/home/home_widgets/home_options_tap_bar/options_button_design.dart';
+import 'package:score_live/app/features/home/home_widgets/home_options_tap_bar/options_button.dart';
 import 'package:score_live/core/applocalization_context.dart';
 import 'package:score_live/core/enums.dart';
 
@@ -13,6 +13,7 @@ class HomeOptionsTapBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<HomeCubit, HomeState>(
+      buildWhen: (previous, current) => previous.homeOptions != current.homeOptions,
       builder: (context, state) {
         return Padding(
           padding: const EdgeInsets.symmetric(vertical: 12.0),
@@ -24,26 +25,26 @@ class HomeOptionsTapBar extends StatelessWidget {
                     _homeCubit(context).toggleButton(true, false, false);
                     _homeCubit(context).switchHomeOptions(HomeOptions.upcoming);
                   },
-                  child: OptionsButtonDesign(isActive: state.isUpcomingActive, buttonTitle: context.localizations.homeUpcoming),
+                  child:
+                      OptionsButton(isActive: state.isUpcomingActive, buttonTitle: context.localizations.homeUpcoming),
                 ),
               ),
               Expanded(
                 child: TextButton(
-                  onPressed: () {
-                    _homeCubit(context).toggleButton(false, true, false);
-                    _homeCubit(context).switchHomeOptions(HomeOptions.score);
-                  },
-                  child: OptionsButtonDesign(isActive: state.isScoreActive, buttonTitle: context.localizations.homeScore)
-                ),
+                    onPressed: () {
+                      _homeCubit(context).toggleButton(false, true, false);
+                      _homeCubit(context).switchHomeOptions(HomeOptions.score);
+                    },
+                    child: OptionsButton(isActive: state.isScoreActive, buttonTitle: context.localizations.homeScore)),
               ),
               Expanded(
                 child: TextButton(
-                  onPressed: () {
-                    _homeCubit(context).toggleButton(false, false, true);
-                    _homeCubit(context).switchHomeOptions(HomeOptions.favorites);
-                  },
-                  child: OptionsButtonDesign(isActive: state.isFavoritesActive, buttonTitle: context.localizations.homeFavorites)
-                ),
+                    onPressed: () {
+                      _homeCubit(context).toggleButton(false, false, true);
+                      _homeCubit(context).switchHomeOptions(HomeOptions.favorites);
+                    },
+                    child: OptionsButton(
+                        isActive: state.isFavoritesActive, buttonTitle: context.localizations.homeFavorites)),
               ),
             ],
           ),
@@ -52,4 +53,3 @@ class HomeOptionsTapBar extends StatelessWidget {
     );
   }
 }
-
