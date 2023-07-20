dbade11c169bd6a625571fd20df904524bdf1f46
Jakub Olszewski <kuba.olszewski.0704@icloud.com>
2023-07-20 14:36:29 +0200

    Add minor visual changes to app bar title

--- lib/app/features/home/match_details/match_details_widgets/match_details_title.dart
+++ lib/app/features/home/match_details/match_details_widgets/match_details_title.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:score_live/core/applocalization_context.dart';
+import 'package:score_live/presentation/constants/text_styles.dart';
 
 class MatchDetailsTitle extends StatelessWidget {
   const MatchDetailsTitle(this.title, {super.key});
@@ -8,31 +9,32 @@ class MatchDetailsTitle extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      child: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                title,
-                textAlign: TextAlign.center,
-                style: const TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 16),
-              )
-            ],
-          ),
-          const SizedBox(height: 5),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                context.localizations.currentDay,
-                style: const TextStyle(color: Colors.grey, fontWeight: FontWeight.bold, fontSize: 16),
-              )
-            ],
-          )
-        ],
-      ),
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              title,
+              textAlign: TextAlign.center,
+              style: const CustomTextStyle(fontSize: 14, color: Colors.white, fontWeight: FontWeight.w600),
+            )
+          ],
+        ),
+        const SizedBox(height: 5),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              context.localizations.currentDay,
+              style: const CustomTextStyle(fontSize: 14, color: Colors.grey, fontWeight: FontWeight.w600),
+            )
+          ],
+        )
+      ],
     );
   }
 }
