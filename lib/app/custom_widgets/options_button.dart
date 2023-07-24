28dcf7f3c21d5e5c8240623237f99cfc355d9f36
Jakub Olszewski <kuba.olszewski.0704@icloud.com>
2023-07-24 12:51:03 +0200

    Give tab bar buttons animated opacity when clicked

--- lib/app/custom_widgets/options_button.dart
+++ lib/app/custom_widgets/options_button.dart
@@ -13,22 +13,26 @@ class OptionsButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(bottom: 2.0),
-      decoration: isActive
-          ? const BoxDecoration(
-              border: Border(
-                  bottom: BorderSide(
-              width: 3.0,
-              color: AppColors.mainThemePink,
-            )))
-          : null,
-      child: Text(
-        buttonTitle,
-        style: TextStyle(
-          color: isActive ? Colors.white : AppColors.inactiveTextGrey,
-          fontSize: 16,
-          fontWeight: FontWeight.bold,
+    return AnimatedOpacity(
+      opacity: isActive ? 1.0 : 0.5,
+      duration: const Duration(milliseconds: 750),
+      child: Container(
+        padding: const EdgeInsets.only(bottom: 2.0),
+        decoration: isActive
+            ? const BoxDecoration(
+                border: Border(
+                    bottom: BorderSide(
+                width: 3.0,
+                color: AppColors.mainThemePink,
+              )))
+            : null,
+        child: Text(
+          buttonTitle,
+          style: TextStyle(
+            color: isActive ? Colors.white : AppColors.inactiveTextGrey,
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
         ),
       ),
     );
