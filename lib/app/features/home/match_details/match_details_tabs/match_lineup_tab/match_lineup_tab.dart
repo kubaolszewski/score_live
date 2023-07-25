15cff23b81759909d4c64efcde8631e1a9c50bde
Jakub Olszewski <kuba.olszewski.0704@icloud.com>
2023-07-25 14:10:15 +0200

    Make final changes in line ups code

--- lib/app/features/home/match_details/match_details_tabs/match_lineup_tab/match_lineup_tab.dart
+++ lib/app/features/home/match_details/match_details_tabs/match_lineup_tab/match_lineup_tab.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:score_live/app/features/home/match_details/match_details_tabs/match_lineup_tab/elements/line_ups_display.dart';
+import 'package:score_live/app/features/home/match_details/match_details_tabs/match_lineup_tab/elements/teams_line_ups_display.dart';
 import 'package:score_live/models/line_up_model/line_up_model.dart';
 import 'package:score_live/models/live_match_model/live_match_model.dart';
+import 'package:score_live/presentation/constants/app_colors.dart';
 
 class MatchLineUpTab extends StatelessWidget {
   const MatchLineUpTab({
@@ -15,8 +16,26 @@ class MatchLineUpTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: LineUpsDisplay(lineUps: lineUps),
+    return Column(
+      children: [
+        Container(
+          padding: const EdgeInsets.all(8.0),
+          color: AppColors.listTileGrey,
+          child: ListView(
+            shrinkWrap: true,
+            primary: false,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  TeamsLineUpsDisplay(lineUps: lineUps, isHomeTeam: true),
+                  TeamsLineUpsDisplay(lineUps: lineUps, isHomeTeam: false),
+                ],
+              )
+            ],
+          ),
+        )
+      ],
     );
   }
 }
