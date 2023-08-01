a73e0682f293677428faeadb07ab7766f2b8460b
Jakub Olszewski <kuba.olszewski.0704@icloud.com>
2023-08-01 11:49:11 +0200

    Remove unnecessary extension

--- lib/core/events_validation_ext.dart
+++ lib/core/events_validation_ext.dart
@@ -1,12 +1,9 @@
 import 'package:score_live/models/match_events_model/match_events_model.dart';
 import 'package:score_live/presentation/constants/app_const_variables.dart';
 
-extension EventProperties on MatchEventsModel {
+extension EventsValidation on MatchEventsModel {
   int get teamIdFromEvent => team?.id ?? AppConstVariables.intPlaceholder;
   int get timeElapsed => time?.elapsed ?? AppConstVariables.intPlaceholder;
-}
-
-extension EventsValidation on MatchEventsModel {
   bool validFirstHalfEvents(int teamId) {
     if (team?.id != null && time?.elapsed != null && player?.name != null) {
       if (teamId == teamIdFromEvent && timeElapsed <= 45) return true;
