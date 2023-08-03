7bec797e275123f57c9111504d127d83768f3bbd
Jakub Olszewski <kuba.olszewski.0704@icloud.com>
2023-08-03 15:28:57 +0200

    Remove underline in dropdown button & add minor changes in its design

--- lib/app/features/competiton/competition_widgets/competition_search_bar.dart
+++ lib/app/features/competiton/competition_widgets/competition_search_bar.dart
@@ -6,56 +6,74 @@ import 'package:score_live/app/features/competiton/cubit/competition_cubit.dart'
 import 'package:score_live/core/applocalization_context.dart';
 import 'package:score_live/presentation/constants/app_colors.dart';
 import 'package:score_live/presentation/constants/app_const_variables.dart';
+import 'package:score_live/presentation/constants/text_styles.dart';
 
 class CompetitionSearchBar extends StatelessWidget {
   CompetitionSearchBar({super.key});
 
+  final List<String> searchTypes = <String>['Team', 'League (name)'];
   final TextEditingController searchingController = TextEditingController();
+
   CompetitionCubit _competitionCubit(BuildContext context) => context.read<CompetitionCubit>();
 
   @override
   Widget build(BuildContext context) {
-    double width = AppConstVariables.widthInfinity;
-    String dropdownValue = 'Team';
+    String dropdownValue = searchTypes.first;
 
     return BlocBuilder<CompetitionCubit, CompetitionState>(
       builder: (context, state) {
         return SafeArea(
-          child: SizedBox(
-            width: width,
-            height: 50,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 8.0),
+            child: SizedBox(
+              width: AppConstVariables.widthInfinity,
+              height: 40,
               child: TextField(
-                style: const TextStyle(color: Colors.white),
+                style: const CustomTextStyle(color: Colors.white, fontWeight: FontWeight.w700),
                 decoration: InputDecoration(
-                  border: const OutlineInputBorder(borderSide: BorderSide.none),
-                  prefixIcon: const Icon(
-                    Icons.search,
-                    color: Colors.white,
+                  filled: true,
+                  fillColor: Colors.grey[850],
+                  border: const OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                    borderRadius: BorderRadius.all(Radius.circular(8)),
                   ),
-                  suffixIcon: DropdownButton(
-                    dropdownColor: AppColors.listTileGrey,
-                    value: dropdownValue,
-                    items: <String>['Team', 'League (name)'].map<DropdownMenuItem<String>>((String value) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        child: Text(
-                          value,
-                          style: const TextStyle(color: Colors.white),
+                  prefixIcon: const Icon(Icons.search, color: Colors.white),
+                  suffixIcon: ClipRRect(
+                    borderRadius: BorderRadius.circular(8),
+                    child: Container(
+                      color: AppColors.mainThemePink,
+                      child: ButtonTheme(
+                        alignedDropdown: true,
+                        child: DropdownButton(
+                          borderRadius: BorderRadius.circular(8),
+                          iconEnabledColor: Colors.white,
+                          underline: const SizedBox.shrink(),
+                          dropdownColor: AppColors.mainThemePink,
+                          value: dropdownValue,
+                          items: searchTypes.map<DropdownMenuItem<String>>(
+                            (String value) {
+                              return DropdownMenuItem<String>(
+                                value: value,
+                                child: Text(
+                                  value,
+                                  style: const CustomTextStyle(color: Colors.white, fontWeight: FontWeight.w700),
+                                ),
+                              );
+                            },
+                          ).toList(),
+                          onChanged: (String? value) {
+                            dropdownValue = value!;
+                            _competitionCubit(context).switchSearching(dropdownValue);
+                          },
                         ),
-                      );
-                    }).toList(),
-                    onChanged: (String? value) {
-                      dropdownValue = value!;
-                      _competitionCubit(context).switchSearching(dropdownValue);
-                    },
+                      ),
+                    ),
                   ),
                   hintText: context.localizations.searchBarHint,
-                  hintStyle: const TextStyle(color: Colors.white, fontSize: 14),
+                  hintStyle: const CustomTextStyle(fontSize: 14, color: Colors.white, fontWeight: FontWeight.w700),
                 ),
-                controller: searchingController,
                 cursorColor: Colors.white,
+                controller: searchingController,
                 onSubmitted: (nameQuery) {
                   switch (dropdownValue) {
                     case 'Team':
