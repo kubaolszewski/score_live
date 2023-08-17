ad96cb16159a7219b08ccf933946ce731f0003c2
Jakub Olszewski <kuba.olszewski.0704@icloud.com>
2023-08-17 13:20:03 +0200

    Update repository module

--- lib/app/di/di_repository_module.dart
+++ lib/app/di/di_repository_module.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:score_live/repositories/competition/competition_screen_repository.dart';
+import 'package:score_live/repositories/competition/searched_result_details_repository.dart';
 import 'package:score_live/repositories/home/home_screen_repository.dart';
 import 'package:score_live/repositories/home/match_details_repository.dart';
 
@@ -16,6 +17,10 @@ class DIRepositoryModule extends Module {
         ),
         Bind.lazySingleton<CompetitionScreenRepository>(
           (i) => CompetitionScreenRepository(i()),
+          export: true,
+        ),
+        Bind.lazySingleton<SearchedResultDetailsRepository>(
+          (i) => SearchedResultDetailsRepository(i()),
           export: true,
         ),
       ];
