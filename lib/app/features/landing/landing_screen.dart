8096e5f24cad97622c07ed934276c69e9cdcd254
Jakub Olszewski <kuba.olszewski.0704@icloud.com>
2023-08-22 10:11:11 +0200

    Rebuild landing view as hook widget, delete unnecessary cubit

--- lib/app/features/landing/landing_screen.dart
+++ lib/app/features/landing/landing_screen.dart
@@ -1,8 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_modular/flutter_modular.dart';
-import 'package:score_live/app/features/landing/cubit/landing_cubit.dart';
-import 'package:score_live/app/features/landing/landing_module.dart';
 import 'package:score_live/app/features/landing/landing_view.dart';
 
 class LandingScreen extends StatelessWidget {
@@ -10,27 +6,8 @@ class LandingScreen extends StatelessWidget {
     super.key,
   });
 
-  void _landingScreenListener(BuildContext context, LandingState state) {
-    switch (state.currentIndex) {
-      case 0:
-        Modular.to.pushNamed(LandingScreenPaths.homeModulePath);
-      case 1:
-        Modular.to.pushNamed(LandingScreenPaths.competitionModulePath);
-      case 2:
-        Modular.to.pushNamed(LandingScreenPaths.accountModulePath);
-      default:
-        Modular.to.pushNamed(LandingScreenPaths.homeModulePath);
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => Modular.get<LandingCubit>(),
-      child: BlocListener<LandingCubit, LandingState>(
-        listener: _landingScreenListener,
-        child: const LandingView(),
-      ),
-    );
+    return const LandingView();
   }
 }
