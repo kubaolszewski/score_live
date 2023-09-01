9cf542979b9a008834275beb62b3998702a48334
Jakub Olszewski <kuba.olszewski.0704@icloud.com>
2023-09-01 10:16:04 +0200

    Rewrite extension from function to getter

--- lib/app/common/extensions/enums/enums_formatter_ext.dart
+++ lib/app/common/extensions/enums/enums_formatter_ext.dart
@@ -2,13 +2,8 @@ import 'package:score_live/app/common/extensions/enums.dart';
 import 'package:score_live/presentation/constants/app_const_variables.dart';
 
 extension EnumsFormatter on StatsSwitch {
-  String statsSwitchHeaderFormatter() {
-    if (name == 'goals') {
-      return AppConstVariables.goalsHeader;
-    }
-    if (name == 'assists') {
-      return AppConstVariables.assistsHeader;
-    }
-    return '';
-  }
+  String get statsSwitchHeaderFormatter => switch (this) {
+        StatsSwitch.goals => AppConstVariables.goalsHeader,
+        StatsSwitch.assists => AppConstVariables.assistsHeader,
+      };
 }
