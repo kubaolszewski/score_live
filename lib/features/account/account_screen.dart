d11e5ea8517f0a75267b31137e725f34a6593223
Jakub Olszewski <kuba.olszewski.0704@icloud.com>
2023-09-27 09:47:12 +0200

    Pass cubit to provider via modular.get

--- lib/features/account/account_screen.dart
+++ lib/features/account/account_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 
 import 'account_view.dart';
 import 'cubit/account_cubit.dart';
@@ -12,7 +13,7 @@ class AccountScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context) => AccountCubit(),
+      create: (context) => Modular.get<AccountCubit>(),
       child: const AccountView(),
     );
   }
